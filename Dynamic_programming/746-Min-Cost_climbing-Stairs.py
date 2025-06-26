ccd011657bc651424b0d849408f8b24bb4a99811
Nickolas Xisto Machado <cabocanick@gmail.com>
2025-06-26 19:47:10 -0300

    problem solved (with relative bad time)

--- Dynamic_programming/746-Min-Cost_climbing-Stairs.py
+++ Dynamic_programming/746-Min-Cost_climbing-Stairs.py
@@ -1,12 +1,20 @@
 from typing import List
 
 class Solution:
-    dp =  dict()
+    def __init__(self):
+        self.dp = dict()
+
+    def minCost(self, cost: List[int], n: int) -> int:
+        if n < 0:
+            return 0
+        if n in self.dp:
+            return self.dp[n]
+        self.dp[n] = cost[n] + min(self.minCost(cost, n - 1), self.minCost(cost, n - 2))
+        return self.dp[n]
+
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
-        if n <2:
-            return cost[0]
-    
+        return min(self.minCost(cost, n - 1), self.minCost(cost, n - 2))
 
-arr = [10,15,20]
+arr = [10, 15, 20]
 print(Solution().minCostClimbingStairs(arr))
